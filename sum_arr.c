28bb7a4ec8c74ba1aadaffcb07e838597bb1e6ae
Tibriz Riad <tibrizriad076@gmail.com>
2021-09-28 20:34:12 +0600

    second file added

--- sum_arr.c
+++ sum_arr.c
@@ -17,3 +17,11 @@ for(i=0;i<n;i++)
 printf("sum of the elements of this array is %d", sum(arr, n));
 return 0;
 }
+int sum(int arr[], int n) {
+int s = 0;
+int i;
+for (i=0;i<n;i++){
+s += arr[i];
+}
+return s;
+}
